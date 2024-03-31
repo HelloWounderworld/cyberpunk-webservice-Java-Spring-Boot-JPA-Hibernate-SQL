35be815829f84671336733f9a7c832623e879205
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2024-03-31 15:59:46 -0300

    Atualizando a minha nota

---
+++ src/main/java/com/cyberpunkdesignbank/cyberpunk/CyberpunkApplication.java
@@ -0,0 +1,13 @@
+package com.cyberpunkdesignbank.cyberpunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberpunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyberpunkdesignbank/cyberpunk/CyberpunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberpunkdesignbank.cyberpunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberpunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
